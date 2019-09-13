27635df7e611f6ed9b392abd60ed8cb991443c8c
pawellewarski <pawellewarski@gmail.com>
2019-09-13 17:14:01 +0200

    first commit

---
+++ src/main/java/pl/lewarski/starwarsrest/StarWarsRestApplication.java
@@ -0,0 +1,13 @@
+package pl.lewarski.starwarsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsRestApplication.class, args);
+	}
+
+}
